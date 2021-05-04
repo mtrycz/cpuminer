7e5bb95d857cc249567a17f01114628a1f737be0
mtrycz <foma84@yahoo.it>
2021-05-04 09:44:43 +0200

    add run script

---
+++ run_miner.sh
@@ -0,0 +1,11 @@
+./minerd \
+ -a sha256d \
+ -o 192.168.1.102:38332 \
+ -p 1qaz2wsx3edc4rfv5tgb \
+ -u vestal \
+ --coinbase-sig="Imma firin mah lazors \\BCHN\\" \
+ --coinbase-addr=1KHEVZ2w5CbXDFTpKBitqCHT8mHd1qbeTV \
+ -t 1 \
+ $@
+
+
